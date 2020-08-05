5ac32a4d5407f6504ff11a040dd4a49654e4e63d
msimon42 <msimon@uab.edu>
2020-08-04 18:53:14 -0600

    Add post table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_04_044643) do
+ActiveRecord::Schema.define(version: 2020_08_05_005202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "text"
+    t.bigint "sub_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["sub_id"], name: "index_posts_on_sub_id"
+  end
 
   create_table "subs", force: :cascade do |t|
     t.string "name"
@@ -21,4 +30,5 @@ ActiveRecord::Schema.define(version: 2020_08_04_044643) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "posts", "subs"
 end
