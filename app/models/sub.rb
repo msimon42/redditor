7f7700a9cd09efd37846fc8b8ff8076f9dcd2725
msimon42 <msimon@uab.edu>
2020-08-05 11:25:20 -0600

    Add method that returns top posts

--- app/models/sub.rb
+++ app/models/sub.rb
@@ -7,4 +7,8 @@ class Sub < ApplicationRecord
     data[:posts].each{|post| Post.create(title: post.title, text: post.selftext, author: post.author, sub_id: self.id, score: (post.ups.to_i - post.downs.to_i))}
     data[:comments].each{|comment| Comment.create(text: comment.body, author: comment.author, sub_id: self.id, score: (comment.ups.to_i - comment.downs.to_i))}
   end
+
+  def top_posts(amt)
+    posts.order(score: :desc).limit(amt)
+  end
 end
