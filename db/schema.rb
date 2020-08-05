a42313f0b27c35fbc7bbb542fcff989c010f2979
msimon42 <msimon@uab.edu>
2020-08-04 18:55:38 -0600

    Add comment table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_005202) do
+ActiveRecord::Schema.define(version: 2020_08_05_005434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "text"
+    t.bigint "sub_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["sub_id"], name: "index_comments_on_sub_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -30,5 +38,6 @@ ActiveRecord::Schema.define(version: 2020_08_05_005202) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "subs"
   add_foreign_key "posts", "subs"
 end
