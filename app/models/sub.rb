c9b07b5468b57f3a37208cfc76d221df0ec9a4d7
msimon42 <msimon@uab.edu>
2020-08-05 16:16:12 -0600

    Add submission is to create process

--- app/models/sub.rb
+++ app/models/sub.rb
@@ -4,8 +4,8 @@ class Sub < ApplicationRecord
 
   def collect_posts_and_comments
     data = RedditService.new.collect_posts_and_comments(self.name)
-    data[:posts].each{|post| Post.create(title: post.title, text: post.selftext, author: post.author, sub_id: self.id, score: (post.ups.to_i - post.downs.to_i))}
-    data[:comments].each{|comment| Comment.create(text: comment.body, author: comment.author, sub_id: self.id, score: (comment.ups.to_i - comment.downs.to_i))}
+    data[:posts].each{|post| Post.create(title: post.title, text: post.selftext, author: post.author, sub_id: self.id, score: (post.ups.to_i - post.downs.to_i), submission_id: post.id)}
+    data[:comments].each{|comment| Comment.create(text: comment.body, author: comment.author, sub_id: self.id, score: (comment.ups.to_i - comment.downs.to_i), submission_id: comment.id)}
   end
 
   def top_posts(amt)
