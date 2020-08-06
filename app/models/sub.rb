fe67061fb13a271cff8d5bd0138c0ed4933ed28b
msimon42 <msimon@uab.edu>
2020-08-06 10:38:00 -0600

    Add error handling for null byte

--- app/models/sub.rb
+++ app/models/sub.rb
@@ -5,7 +5,11 @@ class Sub < ApplicationRecord
   def collect_posts_and_comments
     data = RedditService.new.collect_posts_and_comments(self.name)
     data[:posts].each{|post| Post.create(title: post.title, text: post.selftext, author: post.author, sub_id: self.id, score: (post.ups.to_i - post.downs.to_i), submission_id: post.id)}
-    data[:comments].each{|comment| Comment.create(text: comment.body, author: comment.author, sub_id: self.id, score: (comment.ups.to_i - comment.downs.to_i), submission_id: comment.id)}
+    begin
+      data[:comments].each{|comment| Comment.create(text: comment.body, author: comment.author, sub_id: self.id, score: (comment.ups.to_i - comment.downs.to_i), submission_id: comment.id)}
+    rescue ArgumentError
+      puts 'Failed to collect comment due to argument error'
+    end    
   end
 
   def top_posts(amt)
