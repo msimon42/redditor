512473b55290d9add188fd5cdab9d31b498a34fa
msimon42 <msimon@uab.edu>
2020-08-11 19:36:19 -0600

    Add upvote and downvote methods

--- app/models/generic_submission_model.rb
+++
@@ -1,5 +0,0 @@
-class GenericSubmissionModel
-
-
-
-end  
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :sub
+
+  def upvote
+    RedditBotService.new.vote_by_fullname(self.submission_id, 'upvote')
+  end
+
+  def downvote
+    RedditBotService.new.vote_by_fullname(self.submission_id, 'downvote')
+  end   
 end
