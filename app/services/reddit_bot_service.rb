bfa3f7b8ce01fcacb22e76d956927a6290839208
msimon42 <msimon@uab.edu>
2020-08-11 18:57:19 -0600

    Initialize reddit session

--- app/services/reddit_bot_service.rb
+++ app/services/reddit_bot_service.rb
@@ -1,3 +1,11 @@
 class RedditBotService
-
-end  
+  def initialize
+    @session = Redd.it(
+      user_agent: 'RedditorMan:v1',
+      client_id:  ENV['REDDIT_CLIENT_ID'],
+      secret:     ENV['REDDIT_CLIENT_SECRET'],
+      username:   ENV['REDDIT_USERNAME'],
+      password:   ENV['REDDIT_PASSWORD']
+)
+  end
+end
