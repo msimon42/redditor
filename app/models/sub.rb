4791c4bf8acaa2394a7795274f098cf352b7ecd3
msimon42 <msimon@uab.edu>
2020-08-11 20:26:34 -0600

    Fix to reflect rename

--- app/models/sub.rb
+++ app/models/sub.rb
@@ -3,13 +3,13 @@ class Sub < ApplicationRecord
   has_many :comments
 
   def collect_posts_and_comments
-    data = RedditService.new.collect_posts_and_comments(self.name)
+    data = RedditScrapeService.new.collect_posts_and_comments(self.name)
     data[:posts].each{|post| Post.create(title: post.title, text: post.selftext, author: post.author, sub_id: self.id, score: (post.ups.to_i - post.downs.to_i), submission_id: post.id)}
     begin
       data[:comments].each{|comment| Comment.create(text: comment.body, author: comment.author, sub_id: self.id, score: (comment.ups.to_i - comment.downs.to_i), submission_id: comment.id)}
     rescue ArgumentError
       puts 'Failed to collect comment due to argument error'
-    end    
+    end
   end
 
   def top_posts(amt)
