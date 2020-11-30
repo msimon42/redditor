e99ba8e7598aa56f9bb8522727211d21c4fa1e5d
msimon42 <msimon@uab.edu>
2020-11-29 22:43:38 -0600

    start post method

--- app/services/opentext_service.rb
+++ app/services/opentext_service.rb
@@ -7,8 +7,14 @@ class OpenTextService
     end
 
     def connection
-      Faraday.new(url: opentext_endpoint) do |f|
+      Faraday.new(url: opentext_url) do |f|
         f.adapter Faradat.default_adapter
+      end
+    end
+
+    def post_to_ca
+      connection.post('/rs/v2') do |req|
+        
       end
     end
 
