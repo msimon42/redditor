2b00e8da85e1c4be12a45bb2290bf542680689ea
msimon42 <msimon@uab.edu>
2020-11-29 22:26:24 -0600

    add connection

--- app/services/opentext_service.rb
+++ app/services/opentext_service.rb
@@ -1,5 +1,15 @@
 class OpenTextService
 
+  private
 
+    def opentext_url
+      'https://developer.opentext.com'
+    end
 
-end  
+    def connection
+      Faraday.new(url: opentext_endpoint) do |f|
+        f.adapter Faradat.default_adapter
+      end
+    end
+
+end
