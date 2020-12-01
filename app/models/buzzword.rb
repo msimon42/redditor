45ba523b2f816f5542eb7c1b29f6d0539e8262eb
msimon42 <msimon@uab.edu>
2020-12-01 15:48:49 -0600

    Start get_score method

--- app/models/buzzword.rb
+++ app/models/buzzword.rb
@@ -1,2 +1,18 @@
 class Buzzword < ApplicationRecord
+  def self.get_score(word_list)
+    values = ActiveRecord::Base.connection.execute(
+      """
+        SELECT value FROM buzzwords
+        WHERE #{convert_to_like(word_list)}
+
+      """
+    )
+
+    
+  end
+
+  private
+    def self.convert_to_like(list)
+      list.map{ |word| "word = '#{word}'"  }.join(' OR ')
+    end
 end
