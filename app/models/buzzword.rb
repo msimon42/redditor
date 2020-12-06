387fe72f548eca06a4e92566c74b9b8f7cb1fae4
msimon42 <msimon@uab.edu>
2020-12-05 19:20:02 -0600

    Update query

--- app/models/buzzword.rb
+++ app/models/buzzword.rb
@@ -2,13 +2,13 @@ class Buzzword < ApplicationRecord
   def self.get_score(word_list)
     values = ActiveRecord::Base.connection.execute(
       """
-        SELECT value FROM buzzwords
+        SELECT SUM(value) AS score FROM buzzwords
         WHERE #{convert_to_like(word_list)}
 
       """
     )
 
-    values.sum{|v| v['value']}
+   return values.first['score'] || 0
   end
 
   private
